7459956e53beff054ed0a9a8c9eef91a67fa8d47
ignaciogarcia <ig@pep.security>
2020-08-17 17:32:02 +0200

    Step 4 of codelab.

--- app/src/main/java/com/materialstudies/reply/ui/home/HomeFragment.kt
+++ app/src/main/java/com/materialstudies/reply/ui/home/HomeFragment.kt
@@ -17,15 +17,19 @@
 package com.materialstudies.reply.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.ItemTouchHelper
+import com.google.android.material.transition.MaterialElevationScale
 import com.materialstudies.reply.R
 import com.materialstudies.reply.data.Email
 import com.materialstudies.reply.data.EmailStore
@@ -72,7 +76,6 @@ class HomeFragment : Fragment(), EmailAdapter.EmailAdapterListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO: Set up postponed enter transition.
 
         // Only enable the on back callback if this home fragment is a mailbox other than Inbox.
         // This is to make sure we always navigate back to Inbox before exiting the app.
@@ -92,12 +95,22 @@ class HomeFragment : Fragment(), EmailAdapter.EmailAdapterListener {
         EmailStore.getEmails(args.mailbox).observe(viewLifecycleOwner) {
             emailAdapter.submitList(it)
         }
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
     }
 
     override fun onEmailClicked(cardView: View, email: Email) {
-        // TODO: Set up MaterialElevationScale transition as exit and reenter transitions.
+        exitTransition = MaterialElevationScale(false).apply {
+            duration = resources.getInteger(R.integer.reply_motion_duration_large).toLong()
+        }
+        reenterTransition = MaterialElevationScale(true).apply {
+            duration = resources.getInteger(R.integer.reply_motion_duration_large).toLong()
+        }
+
+        val emailCardDetailTransitionName = getString(R.string.email_card_detail_transition_name)
+        val extras = FragmentNavigatorExtras(cardView to emailCardDetailTransitionName)
         val directions = HomeFragmentDirections.actionHomeFragmentToEmailFragment(email.id)
-        findNavController().navigate(directions)
+        findNavController().navigate(directions, extras)
     }
 
     override fun onEmailLongPressed(email: Email): Boolean {
